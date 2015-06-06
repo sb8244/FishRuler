f793271f34042f4aa04ef83f34baf666e44325ee
Stephen Bussey <steve.bussey@salesloft.com>
2015-06-06 11:31:09 -0400

    xcode init

---
+++ fishackathon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fishackathon
+//
+//  Created by Stephen Bussey on 6/6/15.
+//  Copyright (c) 2015 Stephen Bussey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fishackathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fishackathon
+//
+//  Created by Stephen Bussey on 6/6/15.
+//  Copyright (c) 2015 Stephen Bussey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
